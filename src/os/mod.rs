f1a521e618b43de8cd046b3d76103ace1e91069a
SickleFire <sicklefirex@gmail.com>
2026-06-03 21:31:59 +0800

    commented provider in mod
    ref issue: #23

--- src/os/mod.rs
+++ src/os/mod.rs
@@ -4,12 +4,21 @@ mod windows;
 #[cfg(target_os = "windows")]
 pub use windows::*;
 
+//pub use windows::WindowsMemory as PlatformMemory;
+
 #[cfg(target_os = "linux")]
 mod linux;
+
 #[cfg(target_os = "linux")]
 pub use linux::*;
 
+//pub use linux::LinuxMemory as PlatformMemory;
+
 use crate::types::{HeapBlock, ModuleInfo, Region};
+
+//pub fn provider() -> PlatformMemory {
+//    PlatformMemory
+//}
 
 pub trait MemoryProvider {
     fn walk_regions(&self, pid: u32) -> Vec<Region>;
